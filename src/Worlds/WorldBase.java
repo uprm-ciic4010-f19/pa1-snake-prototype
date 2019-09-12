d4dd4820225114936c738c2e1e5c28a4227a6e7b
Jmax-art <47719028+Jmax-art@users.noreply.github.com>
2019-09-12 17:58:35 -0400

    Fixed score

--- src/Worlds/WorldBase.java
+++ src/Worlds/WorldBase.java
@@ -53,8 +53,8 @@ public abstract class WorldBase {
 
     public void render(Graphics g){
 
-    	g.setColor(Color.white);
-    	g.drawString("score: " + player.score, 9, 50);
+//    	g.setColor(Color.white);
+//    	g.drawString("score: " + player.score1, 9, 50);
         for (int i = 0; i <= 800; i = i + GridPixelsize) {
 
             g.setColor(new Color(128, 0, 128));
